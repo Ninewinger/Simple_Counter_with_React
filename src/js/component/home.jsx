3fb5c0279239061885164b1e3b9dcc67446941bd
Ninewinger <d.v.petricio@live.com>
2021-08-28 01:38:37 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const SimpleCounter = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="bigCounter">
+				<i class="far fa-clock"></i>
+			</div>
+			<div className="six">651651</div>
+			<div className="five"></div>
+			<div className="fourth"></div>
+			<div className="three"></div>
+			<div className="two"></div>
+			<div className="one"></div>
 		</div>
 	);
 };
---
+++ src/js/icons.js
@@ -0,0 +1,6 @@
+import fontawesome from "@fortawesome/fontawesome";
+import faClock from "@fortawesome/fontawesome-free-regular/faCheck";
+fontawesome.confin = {
+	autoReplaceSvg: "nest"
+};
+fontawesome.library.add(faClock);
